50d6f23ca8fd7f720b83edf4fb49b9148a725f7c
beefpotatochips <jess@abysmal.dev>
2024-07-29 12:41:22 +0100

    feat(hook): add functionality to useCaesarCypher hook

--- src/hooks/useCaesarCypher.ts
+++ src/hooks/useCaesarCypher.ts
@@ -2,7 +2,7 @@
 
 interface CaesarCypherResult {
   result: string;
-  error: string | null;
+  error: Error | null;
 }
 
 interface CaesarCypherProps {
@@ -10,13 +10,42 @@ interface CaesarCypherProps {
   offset: number;
 }
 
+const REGEX_CAPTURE = /[a-zA-Z]/g;
+
 function useCaesarCypher({ string, offset }: CaesarCypherProps): CaesarCypherResult {
-  string.toLowerCase();
-  offset.toFixed(0);
+  if (isNaN(offset)) {
+    return {
+      result: '',
+      error: new Error('Offset is not a number')
+    };
+  }
+
+  if (string === '') {
+    return {
+      result: '',
+      error: null
+    };
+  }
+
+
+  const replacedString = string.replace(REGEX_CAPTURE, (char) => {
+    const charCode = char.charCodeAt(0);
+    let newCharCode = charCode;
+
+    // We only want to deal with letters, punctuation & non-utf8 characters should be left as is.
+    if (charCode >= 65 && charCode <= 90) { // Uppercase letters
+      newCharCode = ((charCode - 65 + offset) % 26) + 65;
+    } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
+      newCharCode = ((charCode - 97 + offset) % 26) + 97;
+    }
+
+    return String.fromCharCode(newCharCode);
+  });
+
 
   return {
-    result: '',
-    error: ''
+    result: replacedString,
+    error: null
   };
 }
 
